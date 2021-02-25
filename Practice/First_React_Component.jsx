09880a615233abda4ebdafe5a2fb81d8cfa816c9
prasad145 <acharyaprasad145@gmail.com>
2021-02-25 09:53:01 +0530

    JSX and React Components

---
+++ Practice/First_React_Component.jsx
@@ -0,0 +1,15 @@
+class MyComponent extends React.Component{
+    constructor(props){
+        super(props);
+    };
+
+    render(){
+        return(
+            <div>
+                <h1>My First React Component</h1>
+            </div>
+        );
+    };
+}
+
+ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
---
+++ Practice/Nesting_components.jsx
@@ -0,0 +1,33 @@
+class Fruits extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h2>Fruits:</h2>
+          { /* NonCirus and Citrus components are defined behind the scenes*/ }
+            <NonCitrus/>
+            <Citrus/>
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
+  
+  class TypesOfFood extends React.Component {
+    constructor(props) {
+       super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+          { /* Change code below this line */ }
+          <Fruits/>
+          { /* CVegetables component defined behind the scenes*/ }
+          <Vegetables />
+        </div>
+      );
+    }
+  };
